af527627861f797c90c20ce732125e6da9635f12
ArcaneIllumination <67559098+B-Verschell@users.noreply.github.com>
2021-05-20 19:13:54 -0400

    Create bingoFooter.js

---
+++ src/components/bingoFooter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Typography, Grid} from '@material-ui/core';
+
+function bingoFooter () {
+    return (
+        <Grid container justify="center" alignItems="center">
+        <Typography gutterBottom>
+          Thanks to SpacePegasus for inspiring this idea
+      </Typography>
+      </Grid>
+
+    )
+}
+
+export default bingoFooter;
