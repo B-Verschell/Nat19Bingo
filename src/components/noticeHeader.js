0c6d6b10a19b0501096b5d077ccb860d3eade33c
ArcaneIllumination <67559098+B-Verschell@users.noreply.github.com>
2021-05-20 19:22:42 -0400

    Update noticeHeader.js

--- src/components/noticeHeader.js
+++ src/components/noticeHeader.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Typography, Grid} from '@material-ui/core';
 
-function noticeheader () {
+function noticeHeader () {
     return (
         <Grid container justify="center" alignItems="center">
-        <Typography variant='h1' gutterBottom>
+        <Typography gutterBottom>
           A reminder to the players/gm: please do not do things to purposefully gain bingo sqaures.
       </Typography>
       </Grid>
