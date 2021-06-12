8b1bca0fac7997ca021ba1225005c88689ed8a28
ArcaneIllumination <67559098+B-Verschell@users.noreply.github.com>
2021-06-12 15:11:20 -0400

    Update noticeHeader.js

--- src/components/noticeHeader.js
+++ src/components/noticeHeader.js
@@ -3,17 +3,16 @@ import { Typography, Grid} from '@material-ui/core';
 
 function noticeHeader () {
     return (
+
         <Grid container justify="center" alignItems="center">
         <Typography gutterBottom>
-            <p>
+
         Players/DM/Chat please let bingo squares be organically occuring. 
-        </p>
         <p>
       Do not force or attempt to cause players to match them.
       </p>
       </Typography>
       </Grid>
-
     )
 }
 
