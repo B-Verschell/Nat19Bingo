66773a6a412956184e79465e87bbf870ef8ce472
ArcaneIllumination <67559098+B-Verschell@users.noreply.github.com>
2021-11-04 18:57:26 -0400

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
 import {Link} from 'react-router-dom'
+import './home.css';
 
 function home(){
     return(
@@ -8,16 +9,20 @@ function home(){
             <center style={{paddingTop:"10px"}}>
                 <h1>Pick the campaign to generate a bingo card for</h1>
                 Campaigns that do not have a bingo card are disabled (non-current campagins can still be played using the VODs once they have a card)
-            </center>
-            <Button varriant="link" className="btn btn-primary btn-lg btn-block"><Link to ={`/nat19bingo/vestige`}>Vestige of Ophiuchus</Link></Button>
-            <Button varriant="link" className="btn btn-primary btn-lg btn-block"><Link to ={`/nat19bingo/dms`}>Devils Might Surrender</Link></Button>
-            <Button className="btn btn-primary btn-lg btn-block" disabled>Fate/Solar Shadow</Button>
-            <Button className="btn btn-primary btn-lg btn-block" disabled>Digimon Emblem Story</Button>
-            <Button className="btn btn-primary btn-lg btn-block" disabled>Tomb of Annihilation</Button>
-            <Button className="btn btn-primary btn-lg btn-block" disabled>Forgotten Realms: Curse of Strahd</Button>
-            <Button className="btn btn-primary btn-lg btn-block" disabled>Forgotten Realms: Waterdeep Dragon Heist</Button>
-            <Button className="btn btn-primary btn-lg btn-block" disabled>Forgotten Realms: Princes of the Apocalypse</Button>
             
+            <div class="d-grid gap-2">
+            <Link to ={`/nat19bingo/vestige`}className="btn btn-primary"><button className="btn btn-lg"><strong>Vestige of Ophiuchus</strong></button></Link>
+
+            <Link to ={`/nat19bingo/dms`}className="btn btn-primary"><button className="btn btn-lg"><strong>Devils Might Surrender</strong></button></Link>
+            <button className="btn btn-secondary btn-lg" disabled>Fate/Solar Shadow</button>
+            <button className="btn btn-secondary btn-lg btn-block" disabled>Digimon Emblem Story</button>
+            <button className="btn btn-secondary btn-lg btn-block" disabled>Tomb of Annihilation</button>
+            <button className="btn btn-secondary btn-lg btn-block" disabled>Forgotten Realms: Curse of Strahd</button>
+            <button className="btn btn-secondary btn-lg btn-block" disabled>Forgotten Realms: Waterdeep Dragon Heist</button>
+            <button className="btn btn-secondary btn-lg" disabled>Forgotten Realms: Princes of the Apocalypse</button>
+            <button className="btn btn-secondary btn-lg" disabled>Let Them Play Games Streams</button>
+            </div>
+            </center>
         </div>
     )
 }
